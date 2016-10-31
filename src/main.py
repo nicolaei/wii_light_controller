ee59ca8eb91b475d2d6c69ab4473d684e15f5eb8
Nicolas "SliZe" Eide <nicolas@harlemeide.net>
2016-10-31 21:39:12 +0100

    Added wiimote and accelerometer calculations

--- src/main.py
+++ src/main.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
+import time
+
+import cwiid
+from wiimote import Wiimote
+
+
 
 if __name__ == '__main__':
-    pass
+    wiimote = Wiimote()
+
+    while True:
+        acc = wiimote.get_accelerometer()
+        roll = wiimote.get_roll()
+        pitch = wiimote.get_pitch()
+
+        print "X: %.3f Y: %.3f Z: %.3f Roll: %.2f Pitch: %.2f" % (acc[cwiid.X], acc[cwiid.Y], acc[cwiid.Z], roll, pitch)
+        time.sleep(0.1)
+
+    wiimote.close()
+
---
+++ src/wiimote.py
@@ -0,0 +1,61 @@
+import math
+import cwiid
+
+class Wiimote:
+    _wiimote = None
+
+    def __init__(self):
+        self._connect()
+        self._init_mote()
+
+    def _connect(self):
+        while not self._wiimote:
+            try:
+                self._wiimote = cwiid.Wiimote()
+            except RuntimeError:
+                print "Error while connecting, trying again"
+
+        print "Connected"
+
+    def _init_mote(self):
+        rpt_mode = 0
+        rpt_mode ^= cwiid.RPT_ACC  # Turn on accelerometer
+        rpt_mode ^= cwiid.RPT_BTN  # Turn on buttons
+
+        self._wiimote.rpt_mode = rpt_mode
+
+    def close(self):
+        self._wiimote.close()
+
+    def get_accelerometer(self):
+        # cwiid returns an 8-bit value for the accelometer
+        zero = 127
+        scale = 255
+
+        return (
+            float(self._wiimote.state['acc'][cwiid.X] - zero) / scale,
+            float(self._wiimote.state['acc'][cwiid.Y] - zero) / scale,
+            float(self._wiimote.state['acc'][cwiid.Z] - zero) / scale,
+        )
+
+    def get_roll(self):
+        x, y, z = self.get_accelerometer()
+
+        try:
+            roll = math.atan(x / z)
+            if z <= 0:
+                roll += math.pi * (1 if x > 0 else -1)
+
+            return -roll
+        except ZeroDivisionError:
+            return 0
+
+    def get_pitch(self):
+        x, y, z = self.get_accelerometer()
+        roll = self.get_roll()
+
+        try:
+            pitch = math.atan(y / z * math.cos(roll))
+            return pitch
+        except ZeroDivisionError:
+            return 0
