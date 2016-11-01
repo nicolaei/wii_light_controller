c040ed908a9829c911a1009e661e2a16a6d17ad0
Nicolas "SliZe" Eide <nicolas@harlemeide.net>
2016-11-01 16:16:44 +0100

    Made it so lights are controlled by the wii-mote

--- src/main.py
+++ src/main.py
@@ -2,22 +2,64 @@
 import time
 
 import cwiid
+
+import math
+
+from fixtures import LED
 from wiimote import Wiimote
 
+lights = []
+
+
+def get_colors():
+    """
+    :return: Color based on the controllers roll
+    """
+    roll = wiimote.get_roll()
+
+    color_range = math.pi / 3
+    red = max(0, math.cos(roll * color_range))
+    green = max(0, math.sin(roll * color_range))
+    blue = max(0, -math.cos(roll * color_range))
+
+    # print "Colors: %.2f %.2f %.2f" % (red, green, blue)
+
+    return red, green, blue
+
+
+def get_dim():
+    """
+    :return: Dimming modifier between 0 and 1
+    """
+    return 1 - max(wiimote.get_pitch() / (math.pi / 2), 0)
+
+
 def main_loop():
-    pass
+    selected_light = None
+
+    # Yaw controls which light is being controlled
+    for light in lights:
+        # TODO: Select light with yaw
+        selected_light = lights[0]
+
+    # Get colors based on controller roll
+    colors = get_colors()
+    # Get dim modifier based on controller pitch
+    dim_modifier = get_dim()
+
+    selected_light.set_intensity([int(color * dim_modifier * 255) for color in colors])
+
 
 if __name__ == '__main__':
     wiimote = Wiimote()
 
-    while True:
-        acc = wiimote.get_accelerometer()
-        roll = wiimote.get_roll()
-        pitch = wiimote.get_pitch()
-        acceleration = wiimote.get_acceleration()
+    # tmp for testing
+    lights = [LED(1, 4)]
 
-        print "X: %.3f Y: %.3f Z: %.3f\tRoll: %.2f Pitch: %.2f Acc: %.2f" % (acc[cwiid.X], acc[cwiid.Y], acc[cwiid.Z], roll, pitch, acceleration)
-        time.sleep(0.1)
+    while True:
+        main_loop()
+        # Debug info
+        print "Red: %3d Green: %3d Blue: %3d" % (lights[0].data[0], lights[0].data[1], lights[0].data[2])
 
     wiimote.close()
 
